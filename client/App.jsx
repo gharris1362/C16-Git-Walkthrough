c54f05a2900910d8d5cdf8404a7720360b1ed635
Zomievey <haylee_Hunter@aol.com>
2022-04-04 15:50:12 -0500

    haylee's page

--- client/App.jsx
+++ client/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Pizza from "./pages/Pizza.jsx";
+import Haylee from "./pages/Haylee.jsx";
+import Haylee from './pages/Haylee';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/Pizza" element={<Pizza/>}/>
+                <Route path="/Pizza" element={<Pizza />}/>
+                <Route path="/Haylee" element={<Haylee />}/>
             </Routes>
         </BrowserRouter>
     )
