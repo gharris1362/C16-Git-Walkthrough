c1850967b23377f1279e83c1c9d8e90302b98025
gharris1362 <gharris1362@gmail.com>
2022-04-04 15:51:44 -0500

    forcing merge conflict

--- client/pages/Pizza.jsx
+++ client/pages/Pizza.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Pizza = () => {
-
-    return (
-        <>
-        <h1>This is the Pizza Component</h1>
+  return (
+    <>
+      <h1>This is the Pizza Component</h1>
+      <h1>
         lgsdfbjklgfjkdsahbgdfsaljkhbgsdflahkjblgksjdahbfgsldajkfblkjbgsdalkjbgsadkljbgadsfgasdkjlbgkasdjlbkljbagsdbjklgdsalkjbgdasgdskjblalkjbgdsa
-        </>
-    )
-}
+      </h1>
+    </>
+  );
+};
 
 export default Pizza;
